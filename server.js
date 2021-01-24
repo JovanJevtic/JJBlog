2f141ffea30da469125e184c2bad8d8f0af520dc
Jovan Jevtic <jovanjevtic132@gmail.com>
2021-01-24 20:50:33 +0100

    Connected to mongoDB database

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const dotenv = require('dotenv').config()
+const mongoose = require('mongoose');
 
 const app = express();
 
 //* Middlewares
 app.use(express.json());
+
+//* Database connection
+mongoose.connect( process.env.mongoURI , {useUnifiedTopology: true, useNewUrlParser: true});
+const db = mongoose.connection;
+
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database!'));
 
 //* App running
 const PORT = process.env.PORT || 5000;
