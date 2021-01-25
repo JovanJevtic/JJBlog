c8593eee71c4bed724a006ff937da4408d57afd1
Jovan Jevtic <jovanjevtic132@gmail.com>
2021-01-25 20:08:11 +0100

    Heroku push

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv').config()
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 //* App init
 const app = express();
@@ -21,6 +22,22 @@ app.use('/api/blogs', require('./routes/blogs'));
 //* Database connection
 mongoose.connect( process.env.mongoURI , {useUnifiedTopology: true, useNewUrlParser: true});
 const db = mongoose.connection;
+
+//* Static files serve
+let dirname = path.resolve()
+const environment = process.env.NODE_ENV;
+
+if (environment === 'production') {
+  app.use(express.static(path.join(dirname, '/client/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send(`API is running.... in ${environment} mode`)
+  })
+}
 
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database!'));
