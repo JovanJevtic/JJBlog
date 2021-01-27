9b22c40529c5d29aaa208657ebbe32af8802ae6e
Jovan Jevtic <jovanjevtic132@gmail.com>
2021-01-27 23:13:32 +0100

    Home page styling

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -3,7 +3,7 @@ import Moment from 'react-moment';
 const Blog = ({ blog }) => {
 
     return (
-        <div>
+        <div className="blog">
             <h1>{ blog.title }</h1>
             <p>Written by: { blog.author } </p>
             <p>Date: <Moment format="DD/MM/YYYY" date={blog.date} /></p>
--- client/src/components/BlogsList.js
+++ client/src/components/BlogsList.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
+import '../styling/style.css';
 
 const BlogsList = ({ blogs, title }) => {
 
@@ -8,14 +9,16 @@ const BlogsList = ({ blogs, title }) => {
             <h2>{ title }</h2>
             {
                 blogs.map(blog => (
-                    <Link key={blog._id} className="blog-link" to={`/blog/${blog._id}`}>
-                        <div className="blog">
-                            <h1>{ blog.title }</h1>
-                            <p>{ blog.description }</p>
-                            <p>Written by: { blog.author }</p>
-                            <Moment format="YYYY/MM/DD" date={blog.date} />
-                        </div>
-                    </Link>
+                    <div className="blog-preview">
+                        <Link key={blog._id} className="blog-link" to={`/blog/${blog._id}`}>
+                            <div className="blog-preview-container">
+                                <h1 className="blog-title">{ blog.title }</h1>
+                                <p className="blog-description">{ blog.description }</p>
+                                <p className="blog-author">Written by: { blog.author }</p>
+                                <Moment className="blog-date" format="YYYY/MM/DD" date={blog.date} />
+                            </div>
+                        </Link>
+                    </div>
                 ))
             }
         </div>
