b33f8eebd94c4340bdb3dbb76701fa48551084bd
Jovan Jevtic <jovanjevtic132@gmail.com>
2021-01-28 17:57:46 +0100

    React markdown editor added

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -1,9 +1,14 @@
 import Moment from 'react-moment';
+import MDEditor from '@uiw/react-md-editor';
 
 const Blog = ({ blog }) => {
 
+    const markdown = '# Headline <br/> `npm install` '; 
+    
+
     return (
         <div className="blog">
+            <MDEditor.Markdown source={markdown} />
             <h1>{ blog.title }</h1>
             <p>Written by: { blog.author } </p>
             <p>Date: <Moment format="DD/MM/YYYY" date={blog.date} /></p>
