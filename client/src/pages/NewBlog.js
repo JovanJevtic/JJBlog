ec1e0ae44609427718ede4538f3d213617afefef
Jovan Jevtic <jovanjevtic132@gmail.com>
2021-02-01 20:51:28 +0100

    Set markdown preview mode based on device width

--- client/src/pages/NewBlog.js
+++ client/src/pages/NewBlog.js
@@ -1,17 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
 const NewBlog = () => {
 
     const [ value, setValue ] = useState('');
+    const [ previewMode, setPreviewMode ] = useState('');
+
+    const isMobile = window.innerWidth < '750';
+
+    useEffect(() => {
+        isMobile ? setPreviewMode('edit') : setPreviewMode('live')
+    }, [])
+
+    useEffect(() => {
+        console.log(previewMode)
+    }, [previewMode])
 
     return (
         <div>
             <div className="container">
                 <MDEditor
+                    visiableDragbar={true}
+                    minHeight={350}
+                    maxHeight={500}
+                    preview={previewMode}
                     value={value}
                     onChange={setValue}
                 />
+
+                <MDEditor.Markdown source={value} />
             </div>
         </div>
     );
