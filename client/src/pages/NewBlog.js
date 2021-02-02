6bc358d399c089e958c0a308b01b61d9f2ed4584
Jovan Jevtic <jovanjevtic132@gmail.com>
2021-02-02 16:59:22 +0100

    Mobile optimizatiopn

--- client/src/pages/NewBlog.js
+++ client/src/pages/NewBlog.js
@@ -5,30 +5,27 @@ const NewBlog = () => {
 
     const [ value, setValue ] = useState('');
     const [ previewMode, setPreviewMode ] = useState('');
+    const [ editorHeight, setEditorHeight ] = useState(0);
 
     const isMobile = window.innerWidth < '750';
 
     useEffect(() => {
-        isMobile ? setPreviewMode('edit') : setPreviewMode('live')
+        isMobile ? setPreviewMode('edit') : setPreviewMode('live');
+        isMobile ? setEditorHeight(550) : setEditorHeight(350);
     }, [])
-
-    useEffect(() => {
-        console.log(previewMode)
-    }, [previewMode])
 
     return (
         <div>
             <div className="container">
                 <MDEditor
                     visiableDragbar={true}
-                    minHeight={350}
-                    maxHeight={500}
                     preview={previewMode}
                     value={value}
                     onChange={setValue}
+                    height={editorHeight}
+                    minHeight={350}
+                    maxHeight={550}
                 />
-
-                <MDEditor.Markdown source={value} />
             </div>
         </div>
     );
