24d01471691d6376971cc78d2ec051e9a1203c56
Jovan Jevtic <jovanjevtic132@gmail.com>
2021-02-11 21:20:30 +0100

    Proxy

--- server.js
+++ server.js
@@ -3,21 +3,24 @@ const dotenv = require('dotenv').config()
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-const bodyParser = require('body-parser');
 
 //* App init 
 const app = express();
 
 //* Middlewares
-app.use(express.json());
-app.use(bodyParser.json({limit: '50mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 //* Cors
 const allowedOrigins = ['http://localhost:3000', 'http://jevdevs.herokuapp.com/', 'https://jevdevs.herokuapp.com/'];
-app.use(cors({
-    origin: allowedOrigins
-}));
+// app.use(cors({
+//     origin: allowedOrigins
+// }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://jevdevs.herokuapp.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //* Routes
 app.use('/api/blogs', require('./routes/blogs'));
