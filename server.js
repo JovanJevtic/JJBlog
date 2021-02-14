579591392f7dfcee02b561dba3cdf5ec94758229
Jovan Jevtic <jovanjevtic132@gmail.com>
2021-02-14 08:32:35 +0100

    Reverting

--- server.js
+++ server.js
@@ -9,25 +9,15 @@ const sslRedirect = require('heroku-ssl-redirect').default;
 const app = express();
 
 //* Middlewares
+app.use(express.json());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-/* Cors 
-const allowedOrigins = 'https://jevdevs.herokuapp.com/';
+//* Cors
+const allowedOrigins = ['http://localhost:3000', 'https://jevdev.herokuapp.com/'];
 app.use(cors({
     origin: allowedOrigins
 }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-})
-*/
-
-app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 //* Redirect from http to https
 app.use(sslRedirect(['production']));
